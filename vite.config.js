4e6a4266dbe59126be471d2332b1934cd084292d
Ossman <drake@OSSMAN>
2025-02-13 16:37:12 +0100

    ffs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Refactor2/",
+  base: "./", // Ensures correct paths in production
 });
